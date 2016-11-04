a03c2494b3c54d568cfc1ba0cdce6db7c2d22e04
Jordi Vila <vjordi10@gmail.com>
2016-11-04 21:46:17 +0100

    Updated scene Game, updated script Meteor

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -1,32 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
+
 
 public class Meteor : MonoBehaviour {
-	public Transform Planet;
-	int MoveSpeed = 2;
-	int MaxDist = 100;
-	int MinDist = 1;
+	//Const 
+	public const int MAXDIST = 100;
+
+	//GameObjects
+	public Transform planet;
+
+	//UI
+	public Text distT;
+	public Text stateT;
+
+	//Int
+	public int result = 0;
+	public float dist = MAXDIST;
+	public int state;
+	int moveSpeed = 2;
+	//int maxDist = 100;
+	float minDist = 0.5f;
+
+	public int paramA = 0;
+	public int paramB = 0;
+
+	//String
+	public string operation = "defineix funcio";
+
+
+
+
 	// Use this for initialization
 	void Start () {
+		//stateT.text = state.ToString();
+		
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(Planet);
-
-		if(Vector3.Distance(transform.position,Planet.position) >= MinDist){
-
-			transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+		transform.LookAt(planet);
 
 
 
-			if(Vector3.Distance(transform.position,Planet.position) <= MaxDist)
+		if(Vector3.Distance(transform.position,planet.position) >= minDist){
+
+			transform.position += transform.forward*moveSpeed*Time.deltaTime;
+			dist = Vector3.Distance(transform.position,planet.position);
+			distT.text = dist.ToString();
+
+
+
+			if(Vector3.Distance(transform.position,planet.position) <= MAXDIST)
 			{
 				//Here Call any function U want Like Shoot at here or something
+				//Destroy(gameObject);
+				stateT.text = "x";
+
 			} 
 
+
 		}
+		if(Vector3.Distance(transform.position,planet.position) <= minDist)
+		{
+			//Here Call any function U want Like Shoot at here or something
+			//Destroy(gameObject);
+			stateT.text = "Destroyed";
+		} 
 	
 	}
 }
