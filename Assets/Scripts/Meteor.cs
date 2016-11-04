cd094304e50c6477e4cbdd48161f7270c5f87f01
Jordi Vila <vjordi10@gmail.com>
2016-11-04 18:36:02 +0100

    Added basic scripts

---
+++ Assets/Scripts/Meteor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meteor : MonoBehaviour {
+	public Transform Planet;
+	int MoveSpeed = 2;
+	int MaxDist = 100;
+	int MinDist = 1;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(Planet);
+
+		if(Vector3.Distance(transform.position,Planet.position) >= MinDist){
+
+			transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+
+
+
+			if(Vector3.Distance(transform.position,Planet.position) <= MaxDist)
+			{
+				//Here Call any function U want Like Shoot at here or something
+			} 
+
+		}
+	
+	}
+}
