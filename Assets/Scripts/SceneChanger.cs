ed8f146881e0b6972f345a7387c3f799ea2069bd
Arnau <arnau.guadall@uvic.cat>
2016-11-05 08:21:08 +0100

    Home

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChanger : MonoBehaviour {
+
+    public void changeTo(int escena) {
+        Application.LoadLevel(escena);
+    }
+}
